f86dfbf42fd17801beded9580a4b19af935f6e21
Stanco <sleasenco@gmail.com>
2015-06-19 18:08:29 +0300

    User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_inclusion_of :firstname, :in => [true, false]
 
   has_many :reviews
   has_many :products
